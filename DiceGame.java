a7f494d0b0af80c14bc55ea89346139f6f3d9795
T0-0D <tacwingman015@gmail.com>
2021-07-02 13:29:34 +0900

    Won/lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -1,11 +1,25 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame{
     public static void main(String[] args){
         Random r1 = new Random();
         Random r2 = new Random();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scan.next();
+        System.out.println("Hello, " + name + "!");
         System.out.println("Rolling the dice...");
-        System.out.println("Die1: " + (r1.nextInt(6)+1));
-        System.out.println("Die2: " + (r2.nextInt(6)+1));
+        int die1 = r1.nextInt(6) + 1;
+        int die2 = r2.nextInt(6) + 1;
+        System.out.println("Die1: " + die1);
+        System.out.println("Die2: " + die2);
+        System.out.println("Total value: " + (die1 + die2));
+        if(die1 + die2 > 7){
+            System.out.println("You won!");
+        }else{
+            System.out.println("You lose!");
+        }
     }
 }
